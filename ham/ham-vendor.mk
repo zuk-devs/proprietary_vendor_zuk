30a0aa54b078ef563a85fe4e5d06d32bf63a0c43
mx2345 <mclarke2355@gmail.com>
2016-07-11 05:49:34 +0000

    Enable Quick Charge 2.0 [1/2]

--- ham/ham-vendor.mk
+++ ham/ham-vendor.mk
@@ -17,6 +17,7 @@
 PRODUCT_COPY_FILES += \
     vendor/zuk/ham/proprietary/vendor/bin/adsprpcd:system/vendor/bin/adsprpcd \
     vendor/zuk/ham/proprietary/vendor/bin/hci_qcomm_init:system/vendor/bin/hci_qcomm_init \
+    vendor/zuk/ham/proprietary/vendor/bin/hvdcp:system/vendor/bin/hvdcp \
     vendor/zuk/ham/proprietary/vendor/bin/irsc_util:system/vendor/bin/irsc_util \
     vendor/zuk/ham/proprietary/vendor/bin/loc_launcher:system/vendor/bin/loc_launcher \
     vendor/zuk/ham/proprietary/vendor/bin/mm-qcamera-daemon:system/vendor/bin/mm-qcamera-daemon \
