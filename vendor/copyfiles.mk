ea13b167c1538f7a148baf3922952297d1b68cc3
davide <davidevinavil@gmail.com>
2017-08-24 20:08:11 +0200

    reconfigure vendor

--- vendor/copyfiles.mk
+++
@@ -1,8 +0,0 @@
-copy_files := $(subst $(LOCAL_PATH)/,, \
-	$(filter-out %.mk,\
-	$(patsubst ./%,%, \
-	$(shell find $(LOCAL_PATH) -type f -name "*" -and -not -name ".*") \
-	)))
-
-PRODUCT_COPY_FILES += $(foreach file,$(copy_files),\
-    $(LOCAL_PATH)/$(file):$(file))
